06fc57865d94e9ddf9711ceb2bbd6e5b5b40698d
Keith Fung <keith.robert.fung@gmail.com>
2021-04-16 11:44:21 -0400

    ✨ Storybook Setup
    Run yarn sb init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
