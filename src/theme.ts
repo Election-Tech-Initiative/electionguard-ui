c8acf3f433824a2239a14ed926207a49ff38e534
Keith Fung <keith.robert.fung@gmail.com>
2021-04-30 12:15:48 -0400

    🎨 Customize Material UI Theme

---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import { deepPurple, teal } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: teal,
+        secondary: deepPurple,
+    },
+});
+
+export default theme;
