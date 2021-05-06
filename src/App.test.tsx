047a47100e92a957b893cc19b82a1c6c833c8545
Keith Fung <keith.robert.fung@gmail.com>
2021-05-06 15:34:12 -0400

    ✅ Update app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { IntlProvider } from 'react-intl';
 
 import App from './App';
+import { ConfigContext, defaultConfig } from './contexts/config';
+import { LanguageContext, defaultLanguage } from './contexts/language';
 
 test('renders logo', () => {
-    render(<App />);
+    render(
+        <ConfigContext.Provider value={defaultConfig}>
+            <LanguageContext.Provider value={defaultLanguage}>
+                <IntlProvider locale={defaultLanguage.locale} messages={defaultLanguage.messages}>
+                    <App />
+                </IntlProvider>
+            </LanguageContext.Provider>
+        </ConfigContext.Provider>
+    );
     const logos = screen.getAllByText(/electionguard-logo.svg/i);
     const firstLogo = logos[0];
     expect(firstLogo).toBeInTheDocument();
