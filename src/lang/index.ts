36d3af02479d4541bdfe7dd8b3bb384bd21d68bd
Keith Fung <keith.robert.fung@gmail.com>
2021-05-24 09:53:39 -0400

    🔠 Add lorem ipsum text

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -21,3 +21,13 @@ export const getLanguage = (): Language => {
             return supported.english;
     }
 };
+
+export const loremIpsum = `Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit. Etiam a egestas dui. 
+Etiam eget sem quis lorem pharetra viverra. Mauris rhoncus 
+ultricies urna, sit amet bibendum libero gravida et. Praesent 
+dictum sed sapien et auctor. Quisque porttitor purus arcu, ac 
+faucibus mi finibus id. Etiam semper lorem sem. Integer blandit, 
+quam eget accumsan accumsan, ante lorem ullamcorper ipsum, eget
+ egestas lectus dui nec ante. Praesent scelerisque lacinia 
+ purus a lacinia.`;
