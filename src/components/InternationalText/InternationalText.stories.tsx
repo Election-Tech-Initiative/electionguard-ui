0f95f9bdef145ae990238e394d9d2146ed5a57fa
Keith Fung <keith.robert.fung@gmail.com>
2021-05-26 14:01:15 -0400

    ✨Implement InternationalText, wrapper for FormattedMessage
    A typography and formatted message to simplify future code

---
+++ src/components/InternationalText/InternationalText.stories.tsx
@@ -0,0 +1,33 @@
+import { Box } from '@material-ui/core';
+import { Meta, Story } from '@storybook/react';
+import React from 'react';
+
+import { MessageId } from '../../lang';
+import InternationalText, { InternationalTextProps } from './InternationalText';
+
+export default {
+    title: 'Components/InternationalText',
+    component: InternationalText,
+} as Meta;
+
+const Template: Story<InternationalTextProps> = (args) => (
+    <Box display="flex" flexDirection="column" alignItems="center">
+        <InternationalText variant="h1" {...args} />
+        <InternationalText variant="h2" {...args} />
+        <InternationalText variant="h3" {...args} />
+        <InternationalText variant="h4" {...args} />
+        <InternationalText variant="h5" {...args} />
+        <InternationalText variant="h6" {...args} />
+        <InternationalText variant="body1" {...args} />
+    </Box>
+);
+
+export const Standard = Template.bind({});
+Standard.storyName = 'Standard';
+Standard.args = {
+    id: MessageId.Overload,
+    defaultMessage: 'International Text',
+};
+
+export const Placeholder = Template.bind({});
+Placeholder.storyName = 'No text provided';
---
+++ src/components/InternationalText/InternationalText.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { Typography, TypographyProps } from '@material-ui/core';
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import { MessageId } from '../../lang';
+
+export interface InternationalTextProps extends TypographyProps {
+    id?: MessageId;
+    description?: string;
+    defaultMessage?: string;
+    component?: React.ElementType;
+}
+
+/**
+ * International text to handle automatically finding correct language
+ */
+const InternationalText: React.FC<InternationalTextProps> = (props) => {
+    const typographyProps = props as TypographyProps;
+    const { id, description, defaultMessage, component } = props;
+    return (
+        <Typography {...typographyProps} component={component || 'span'}>
+            <FormattedMessage
+                id={id || MessageId.AppPlaceholder}
+                description={description}
+                defaultMessage={defaultMessage || 'placeholder'}
+            />
+        </Typography>
+    );
+};
+
+export default InternationalText;
