3ab1b4c1dc8850e98f2ec304e99fa319afd78bfa
Keith Fung <keith.robert.fung@gmail.com>
2021-06-04 11:21:38 -0400

    🎨 Add colors to theme for guardians

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,50 @@
-import { teal } from '@material-ui/core/colors';
+import {
+    amber,
+    blue,
+    blueGrey,
+    brown,
+    cyan,
+    deepOrange,
+    deepPurple,
+    green,
+    indigo,
+    lightBlue,
+    lightGreen,
+    lime,
+    orange,
+    pink,
+    purple,
+    red,
+    teal,
+    yellow,
+} from '@material-ui/core/colors';
 import { Localization } from '@material-ui/core/locale';
 import { Theme, ThemeOptions, createMuiTheme } from '@material-ui/core/styles';
 
 const midnightBlue = '#002a84';
+
+const colorSet: string[] = [
+    orange[700],
+    pink[700],
+    deepPurple[700],
+    blue[700],
+    cyan[700],
+    amber[700],
+    blueGrey[700],
+    purple[700],
+    red[700],
+    yellow[700],
+    brown[700],
+    deepOrange[700],
+    green[700],
+    indigo[700],
+    lightBlue[700],
+    lightGreen[700],
+    lime[700],
+    teal[700],
+];
+
+export const getColor = (index: number): string => colorSet[index % colorSet.length];
 
 const theme = (localization?: Localization): Theme => {
     const options: ThemeOptions = {
