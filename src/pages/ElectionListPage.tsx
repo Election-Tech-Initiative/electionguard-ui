6639d06c9f1b7d17629dbc68a2d847b355dd1e79
Keith Fung <keith.robert.fung@gmail.com>
2021-06-09 12:04:42 -0400

    More routing

--- src/pages/ElectionListPage.tsx
+++ src/pages/ElectionListPage.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 
 import ElectionTable from '../components/ElectionTable';
 import { Message, MessageId } from '../lang';
 import ListPageLayout from '../layouts/ListPageLayout';
 import getElections from '../mocks/elections';
 
-const ElectionListPage: React.FC = () => (
-    <ListPageLayout
-        title={new Message(MessageId.ElectionListTitle)}
-        description={new Message(MessageId.ElectionListDescription)}
-    >
-        <ElectionTable data={getElections()} />
-    </ListPageLayout>
-);
+const ElectionListPage: React.FC = () => {
+    const history = useHistory();
+    return (
+        <ListPageLayout
+            title={new Message(MessageId.ElectionListTitle)}
+            description={new Message(MessageId.ElectionListDescription)}
+            goHome={() => history.push('/')}
+        >
+            <ElectionTable data={getElections()} />
+        </ListPageLayout>
+    );
+};
 
 export default ElectionListPage;
--- src/pages/JointKeyListPage.tsx
+++ src/pages/JointKeyListPage.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 
 import JointKeyTable from '../components/JointKeyTable';
 import { Message, MessageId } from '../lang';
 import ListPageLayout from '../layouts/ListPageLayout';
 import { getJointKeys } from '../mocks/electionSetup';
 
-const JointKeyListPage: React.FC = () => (
-    <ListPageLayout
-        title={new Message(MessageId.JointKeyList_Title)}
-        description={new Message(MessageId.JointKeyList_Description)}
-    >
-        <JointKeyTable data={getJointKeys()} />
-    </ListPageLayout>
-);
+const JointKeyListPage: React.FC = () => {
+    const history = useHistory();
+    return (
+        <ListPageLayout
+            title={new Message(MessageId.JointKeyList_Title)}
+            description={new Message(MessageId.JointKeyList_Description)}
+            goHome={() => history.push('/')}
+        >
+            <JointKeyTable data={getJointKeys()} />
+        </ListPageLayout>
+    );
+};
 
 export default JointKeyListPage;
