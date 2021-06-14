0b2960e2c33d77d790cf4f201b88e956f6f749ca
Keith Fung <keithrfung@users.noreply.github.com>
2021-06-14 16:03:04 -0400

    ✨ Create A/B testing for languages (#26)
    - Create two additional english language options for testing with storybook

--- src/lang/supported.ts
+++ src/lang/supported.ts
@@ -2,6 +2,8 @@ import { enUS } from '@material-ui/core/locale';
 
 import Language from '../models/language';
 import EnumDictionary from '../utils/EnumDictionary';
+import en_a from './en_a.json';
+import en_b from './en_b.json';
 import en from './en.json';
 import MessageId from './MessageId';
 
@@ -14,6 +16,18 @@ export const supported = {
         name: 'English',
         locale: Locale.English,
         messages: en as EnumDictionary<MessageId, string>,
+        mui: enUS,
+    },
+    englishA: {
+        name: 'English - Version A',
+        locale: Locale.English,
+        messages: en_a as EnumDictionary<MessageId, string>,
+        mui: enUS,
+    },
+    englishB: {
+        name: 'English - Version B',
+        locale: Locale.English,
+        messages: en_b as EnumDictionary<MessageId, string>,
         mui: enUS,
     },
 };
