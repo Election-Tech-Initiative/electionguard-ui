5db86cb51f68ec7867bb9f193628bf955ec1748c
Keith Fung <keith.robert.fung@gmail.com>
2021-06-16 09:51:24 -0400

    ✨ Organize Cells instead of only having on Tables

--- src/components/Cells/index.tsx
+++ src/components/Cells/index.tsx
@@ -3,8 +3,12 @@ import { GridCellParams } from '@material-ui/data-grid';
 import React, { ReactElement } from 'react';
 import { IntlShape } from 'react-intl';
 
+import { Message } from '../../lang';
+import TaskStatus from '../../models/taskStatus';
 import { getColor } from '../../theme';
+import FormattedButton from '../FormattedButton/FormattedButton';
 import GuardianIcon from '../GuardianIcon';
+import TaskStatusIcon from '../TaskStatusIcon/TaskStatusIcon';
 
 export const EmptyCell = (): ReactElement => <></>;
 
@@ -22,4 +26,20 @@ export const GuardianIconCell = (params: GridCellParams): ReactElement => {
     const { value } = params;
     const sequenceOrder = Number(value);
     return <GuardianIcon sequenceOrder={sequenceOrder} color={getColor(sequenceOrder)} />;
+};
+
+export const TaskStatusCell = (params: GridCellParams): ReactElement => {
+    const { value } = params;
+    const status = value as TaskStatus;
+    return <TaskStatusIcon status={status} />;
+};
+
+export const IdButtonCell = (
+    params: GridCellParams,
+    text: Message,
+    onClick: (id: string) => void
+): ReactElement => {
+    const { value } = params;
+    const id = value as string;
+    return <FormattedButton text={text} onClick={() => onClick(id)} />;
 };
