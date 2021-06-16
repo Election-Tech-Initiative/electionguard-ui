6e87df63f534bb3dc98a1c0a01635c5ced66f20c
Keith Fung <keith.robert.fung@gmail.com>
2021-06-16 09:53:01 -0400

    🚧 Add base mocks

--- src/mocks/guardians.ts
+++ src/mocks/guardians.ts
@@ -1,10 +1,11 @@
 import AssignedGuardian from '../models/assignedGuardian';
-import { getColor } from '../theme';
 
 export const getAssignedGuardians = (): AssignedGuardian[] => [
-    { color: getColor(1), sequenceOrder: 1, id: '1', name: 'Snow' },
-    { color: getColor(2), sequenceOrder: 2, id: '2', name: 'Lannister' },
-    { color: getColor(3), sequenceOrder: 3, id: '3', name: 'Magic' },
+    { sequenceOrder: 1, id: '1', name: 'Snow' },
+    { sequenceOrder: 2, id: '2', name: 'Lannister' },
+    { sequenceOrder: 3, id: '3', name: 'Magic' },
+    { sequenceOrder: 4, id: '4', name: 'Stark' },
+    { sequenceOrder: 5, id: '5', name: 'Targaryen' },
 ];
 
 export default getAssignedGuardians;
--- src/mocks/keyCeremony.ts
+++ src/mocks/keyCeremony.ts
@@ -1,24 +1,66 @@
+import KeyCeremonyStep from '../components/KeyCeremonyWizard/KeyCeremonyStep';
 import { KeyCeremony, KeyCeremonyGuardian, KeyCeremonyStatus } from '../models/keyCeremony';
 import TaskStatus from '../models/taskStatus';
 import { getAssignedGuardians } from './guardians';
 
-export const getKeyCeremonies = (): KeyCeremony[] => [
-    {
-        id: 'key-ceremony-1',
-        status: KeyCeremonyStatus.Inactive,
-        name: 'Montgomery County Election',
-        numberOfGuardians: 5,
-        quorum: 3,
-        dateCreated: new Date(),
-        guardians: [],
-    },
-];
-
 export const getKeyCeremonyGuardians = (): KeyCeremonyGuardian[] =>
     getAssignedGuardians().map((guardian) => ({
         ...guardian,
-        sharedPublicKey: TaskStatus.Incomplete,
-        sharedPartialKeyBackups: TaskStatus.Incomplete,
+        keypairCreated: TaskStatus.Incomplete,
+        backupsCreated: TaskStatus.Incomplete,
+        publicKeyShared: TaskStatus.Incomplete,
+        backupsShared: TaskStatus.Incomplete,
         backupsVerified: TaskStatus.Incomplete,
-        readyForCombine: TaskStatus.Incomplete,
+        verifications: [],
     }));
+
+export const getKeyCeremonyGuardiansByStep = (step: KeyCeremonyStep): KeyCeremonyGuardian[] =>
+    getAssignedGuardians().map((guardian) => ({
+        ...guardian,
+        keypairCreated:
+            step > KeyCeremonyStep.CreateKeyPair ? TaskStatus.Complete : TaskStatus.Incomplete,
+        backupsCreated:
+            step > KeyCeremonyStep.CreateBackups ? TaskStatus.Complete : TaskStatus.Incomplete,
+        publicKeyShared:
+            step > KeyCeremonyStep.SharePublicKey ? TaskStatus.Complete : TaskStatus.Incomplete,
+        backupsShared:
+            step > KeyCeremonyStep.ShareBackups ? TaskStatus.Complete : TaskStatus.Incomplete,
+        backupsVerified:
+            step > KeyCeremonyStep.VerifyBackups ? TaskStatus.Complete : TaskStatus.Incomplete,
+        verifications: getAssignedGuardians()
+            .filter((g) => g.id !== guardian.id)
+            .map((g) => ({
+                verifier: g,
+                owner: guardian,
+                verified: TaskStatus.Complete,
+            })),
+    }));
+
+export const getKeyCeremonyGuardiansMidway = (): KeyCeremonyGuardian[] =>
+    getAssignedGuardians().map((guardian) => ({
+        ...guardian,
+        keypairCreated: TaskStatus.Complete,
+        backupsCreated: TaskStatus.Incomplete,
+        publicKeyShared: TaskStatus.Complete,
+        backupsShared: TaskStatus.Complete,
+        backupsVerified: TaskStatus.Incomplete,
+        verifications: getAssignedGuardians()
+            .filter((g) => g.id !== guardian.id)
+            .map((g) => ({
+                verifier: g,
+                owner: guardian,
+                verified: TaskStatus.Complete,
+            })),
+    }));
+
+export const getKeyCeremonies = (): KeyCeremony[] => [
+    {
+        id: 'key-ceremony-1',
+        status: KeyCeremonyStatus.Active,
+        name: 'Montgomery County Election',
+        numberOfGuardians: 5,
+        quorum: 3,
+        guardians: getKeyCeremonyGuardiansMidway(),
+        dateCreated: new Date(),
+    },
+];
