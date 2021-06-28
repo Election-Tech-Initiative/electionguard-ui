3991059cc7c741c6cea6dd91403dc526b99d10a6
Keith Fung <keithrfung@users.noreply.github.com>
2021-06-28 16:31:46 -0400

    🔠  Support for Rich Text on International Text (#31)
    * 🔠  Support for bold, italics, and colored on International Text
    
    International text now support bold, colored and italics.
    
    * 🔠 Example language demonstration

--- src/components/InternationalText/InternationalText.tsx
+++ src/components/InternationalText/InternationalText.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { Typography, TypographyProps } from '@material-ui/core';
+import { Box, Typography, TypographyProps, useTheme } from '@material-ui/core';
 import React from 'react';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { useIntl } from 'react-intl';
 
 import { MessageId, OverloadableMessageId } from '../../lang';
 
@@ -13,29 +13,53 @@ export interface InternationalTextProps extends TypographyProps {
     screenReaderOnly?: boolean;
 }
 
+const bold = (str: string) => (
+    <Box fontWeight="fontWeightMedium" display="inline" component="b">
+        {str}
+    </Box>
+);
+
+const italic = (str: string) => (
+    <Box fontStyle="italics" display="inline" component="i">
+        {str}
+    </Box>
+);
+
+const colored = (str: string, color: string) => (
+    <Box color={color} fontWeight="fontWeightMedium" display="inline" component="b">
+        {str}
+    </Box>
+);
+
 /**
  * International text to handle automatically finding correct language
  */
 const InternationalText: React.FC<InternationalTextProps> = (props) => {
+    const theme = useTheme();
     const intl = useIntl();
     const typographyProps = props as TypographyProps;
     const { id, description, defaultMessage, component, screenReaderOnly } = props;
+    const message = intl.formatMessage(
+        {
+            id: id || MessageId.Placeholder,
+            description,
+            defaultMessage: defaultMessage || 'placeholder',
+        },
+        {
+            bold,
+            italic,
+            primary: (str: string) => colored(str, theme.palette.primary.main),
+            secondary: (str: string) => colored(str, theme.palette.secondary.main),
+        }
+    );
     if (screenReaderOnly) {
         return (
-            <Typography
-                {...typographyProps}
-                component={component || 'span'}
-                aria-label={intl.formatMessage({ id, description, defaultMessage })}
-            />
+            <Typography {...typographyProps} component={component || 'span'} aria-label={message} />
         );
     }
     return (
         <Typography {...typographyProps} component={component || 'span'}>
-            <FormattedMessage
-                id={id || MessageId.Placeholder}
-                description={description}
-                defaultMessage={defaultMessage || 'placeholder'}
-            />
+            {message}
         </Typography>
     );
 };
