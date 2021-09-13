935c6148c85085bfc006f8f4bd3fb8ef1087a53a
SteveMaier-IRT <steve.maier@infernored.com>
2021-09-13 15:25:24 -0400

    Cleaned up mock names

--- packages/api/src/mocks/users.ts
+++ packages/api/src/mocks/users.ts
@@ -1,15 +1,15 @@
 import User from '../models/user';
 
 export const getUsersWithGuardianRole = async (): Promise<User[]> => [
-    { id: '1', name: 'Snow api mock' },
-    { id: '2', name: 'Lannister api mock' },
-    { id: '3', name: 'Magic api mock' },
-    { id: '4', name: 'Stark api mock' },
-    { id: '5', name: 'Targaryen api mock' },
-    { id: '6', name: 'Melisandre api mock' },
-    { id: '7', name: 'Clifford api mock' },
-    { id: '8', name: 'Frances api mock' },
-    { id: '9', name: 'Roxie api mock' },
+    { id: '1', name: 'Snow mock' },
+    { id: '2', name: 'Lannister mock' },
+    { id: '3', name: 'Magic mock' },
+    { id: '4', name: 'Stark mock' },
+    { id: '5', name: 'Targaryen mock' },
+    { id: '6', name: 'Melisandre mock' },
+    { id: '7', name: 'Clifford mock' },
+    { id: '8', name: 'Frances mock' },
+    { id: '9', name: 'Roxie mock' },
 ];
 
 export default getUsersWithGuardianRole;
--- packages/api/src/server/guardians.ts
+++ packages/api/src/server/guardians.ts
@@ -2,11 +2,11 @@ import AssignedGuardian from '../models/assignedGuardian';
 import { post } from '../utils/http';
 
 export const getAssignedGuardians = (): AssignedGuardian[] => [
-    { sequenceOrder: 1, id: '1', name: 'Snow assigned server api' },
-    { sequenceOrder: 2, id: '2', name: 'Lannister assigned server api' },
-    { sequenceOrder: 3, id: '3', name: 'Magic assigned server api' },
-    { sequenceOrder: 4, id: '4', name: 'Stark assigned server api' },
-    { sequenceOrder: 5, id: '5', name: 'Targaryen assigned server api' },
+    { sequenceOrder: 1, id: '1', name: 'Snow server' },
+    { sequenceOrder: 2, id: '2', name: 'Lannister server' },
+    { sequenceOrder: 3, id: '3', name: 'Magic server' },
+    { sequenceOrder: 4, id: '4', name: 'Stark server' },
+    { sequenceOrder: 5, id: '5', name: 'Targaryen server' },
 ];
 
 export const createGuardian = async (id: string, username: string, sequenceOrder:  number): Promise<string> => {
