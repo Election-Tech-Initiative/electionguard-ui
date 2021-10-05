0954f7554806b3f25cea15519ad055354a890670
Keith Fung <keith.robert.fung@gmail.com>
2021-10-05 11:03:13 -0400

    ✅ Fix test after merges

--- packages/result-app/src/App.test.tsx
+++ packages/result-app/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders page', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+    const h1Element = screen.getByText(/select election page/i);
+    expect(h1Element).toBeInTheDocument();
 });
