fe4e9382d87c78376a6505abc1f7abce2647c061
SteveMaier-IRT <steve.maier@infernored.com>
2021-10-15 11:26:10 -0400

    Adding the tally calls to the mediator class Cleaned up unneeded comments

--- packages/api-client/src/GuardianApi.ts
+++ packages/api-client/src/GuardianApi.ts
@@ -12,50 +12,25 @@ import { decryptSharesBallot } from './server/ballots';
 import { decryptShareTally, decryptSharePostTally } from './server/tally';
 
 export default class GuardianApi {
-    // ballot
-    // post /decrypt-shares
     decryptSharesBallot = decryptSharesBallot;
 
-    // guardian
-    // get guardian - coded
-    //  {{guardian-url}}/api/{{version}}/guardian?guardian_id=guardian_1
     guardianGet = getGuardian;
 
-    // get public-keys
-    //  {{guardian-url}}/api/{{version}}/guardian/public-keys?guardian_id=guardian_1
     guardianPublicKeys = getGuardianPublicKeys;
 
-    // post guardian (make guardian) - working
-    //  {{guardian-url}}/api/{{version}}/guardian/backup
-    //  guardian in body
     guardianCreate = createGuardian;
 
-    // post backup
-    //  data in body
     guardianBackup = backupGuardian;
 
-    // post backup/verify
-    //  data in body
     guardianBackupVerification = backupVerificationGuardian;
 
-    // post challege
-    //  data in body
     guardianBackupChallenge = backupChallengeGuardian;
 
-    // post challenge/verity
-    //  data in body
     guardianVerifyChallenge = verifyChallengeGuardian;
 
-    // post find - working
-    //  data in body
     guardianFind = getUsersWithGuardianRole;
 
-    // tally
-    // get decrypt-share
-    //      election_id: str, tally_name: str,
     tallyDecryptShare = decryptShareTally;
 
-    // post decrypt-share
-    //  data in body
     tallyDecryptSharePost = decryptSharePostTally;
 }
--- packages/api-client/src/MediatorApi.ts
+++ packages/api-client/src/MediatorApi.ts
@@ -39,149 +39,113 @@ import {
 } from './server/keyCeremony';
 import { findKeyGuardians, getGuardians, postGuardians, putGuardians } from './server/guardians';
 import { getManifest, findManifest, putManifest, validateManifest } from './server/manifest';
+import {
+    decryptShareTally,
+    decryptSharePostTally,
+    decryptTally,
+    findTally,
+    findTallyDecrypt,
+    getTally,
+    postTally,
+    submitShareTally,
+} from './server/tally';
 
 export default class MediatorApi {
-    // ballot
-    // get ballot
-    // election_id: str, ballot_id: str
     getBallot = getBallot;
 
-    // get inventory
     getBallotInventory = getBallotInventory;
 
-    // post find
     findBallots = findBallots;
 
-    // post cast
     castBallots = castBallots;
 
-    // post spoil
     spoilBallots = spoilBallots;
 
-    // put submit
     submitBallots = submitBallots;
 
-    // post validate
     validateBallot = validateBallot;
 
-    // ballot - decrypt
-    // post decrypt
     decryptBallot = decryptBallot;
 
-    // ballot - encrypt
-    // post encrypt
     encryptBallot = encryptBallot;
 
-    // election
-    // get constants
     getConstants = getConstants;
 
-    // get election
     getElection = getElection;
 
-    // put election
     putElection = putElection;
 
-    // post find
     findElection = findElection;
 
-    // post open
     openElection = openElection;
 
-    // post close
     closeElection = closeElection;
 
-    // post publish
     publishElection = publishElection;
 
-    // post context
     makeContextElection = makeContextElection;
 
-    // key
-    // key - admin
-    // get ceremony
     getKeyCeremonies = getKeyCeremonies;
 
-    // put ceremony
     putCeremony = putKeyCeremony;
 
-    // get ceremony/state
     getKeyCeremonyState = getKeyCeremonyState;
 
-    // post ceremony/find
     findKeyCeremonies = findKeyCeremonies;
 
-    // post ceremony/open
     openKeyCeremony = openKeyCeremony;
 
-    // post ceremony/close
     closeKeyCeremony = closeKeyCeremony;
 
-    // post ceremony/challenge
     challengeKeyCeremony = challengeKeyCeremony;
 
-    // post ceremony/chalenge/verify
     challengeVerifyKeyCeremony = challengeVerifyKeyCeremony;
 
-    // post ceremony/cancel
     cancelKeyCeremony = cancelKeyCeremony;
 
-    // get ceremony/joint_key
     getJointKeyKeyCeremony = getJointKeyKeyCeremony;
 
-    // post ceremony/combine
     combineKeyCeremony = combineKeyCeremony;
 
-    // post ceremony/publish
     publishKeyCeremony = publishKeyCeremony;
 
-    // key - ceremony
-    // post guardian/announce
     announceGuardianKeyCeremony = announceGuardianKeyCeremony;
 
-    // post guardian/backup
     backupGuardianKeyCeremony = backupGuardianKeyCeremony;
 
-    // post guardian/verify
     verifyGuardianKeyCeremony = verifyGuardianKeyCeremony;
 
-    // post guardian/challenge
     challengeGuardianKeyCeremony = challengeGuardianKeyCeremony;
 
-    // guardian
-    // get guardian
     getGuardians = getGuardians;
 
-    // put guardian
     putGuardians = putGuardians;
 
-    // post guardian
     postGuardians = postGuardians;
 
-    // post find
     findKeyGuardians = findKeyGuardians;
 
-    // manifest
-    // get manifest
     getManifest = getManifest;
 
-    // put manifest
     putManifest = putManifest;
 
-    // post find
     findManifest = findManifest;
 
-    // post validate
     validateManifest = validateManifest;
 
-    // tally
-    // get tally
-    // post tally
-    // post find
-    // post decrypt
+    getTally = getTally;
 
-    // tally - decrypt
-    // get tally-derypt
-    // post submit-share
-    // post find
+    postTally = postTally;
+
+    findTally = findTally;
+
+    decryptTally = decryptTally;
+
+    decryptShareTally = decryptShareTally;
+
+    decryptSharePostTally = decryptSharePostTally;
+
+    findTallyDecrypt = findTallyDecrypt;
+
+    submitShareTally = submitShareTally;
 }
