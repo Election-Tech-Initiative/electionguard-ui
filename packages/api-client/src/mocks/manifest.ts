f809a89b56b7ec12b4c5e8c72978a5137548d9e4
SteveMaier-IRT <steve.maier@infernored.com>
2021-11-19 14:19:54 -0500

    add functions for common mock object generation

--- packages/api-client/src/mocks/manifest.ts
+++ packages/api-client/src/mocks/manifest.ts
@@ -2,95 +2,99 @@ import { ElectionManifest } from '../models/election';
 import { ElementModQ } from '../models/keyCeremony';
 import { Manifest } from '../models/manifestPreview';
 
+function generateMockElectionManifest(): ElectionManifest {
+    return {
+        ballot_styles: [
+            {
+                geopolitical_unit_ids: ['hamilton-county', 'congress-district-7'],
+                object_id: 'congress-district-7-hamilton-county',
+            },
+        ],
+        candidates: [
+            {
+                name: {
+                    text: [{ language: 'en', value: 'Joseph Barchi and Joseph Hallaren' }],
+                },
+                object_id: 'barchi-hallaren',
+                party_id: 'whig',
+            },
+            {
+                name: { text: [{ language: 'en', value: 'Adam Cramer and Greg Vuocolo' }] },
+                object_id: 'cramer-vuocolo',
+                party_id: 'federalist',
+            },
+        ],
+        contact_information: {
+            address_line: ['1234 Paul Revere Run', 'Hamilton, Ozark 99999'],
+            email: [
+                { annotation: 'press', value: 'inquiries@hamilton.state.gov' },
+                { annotation: 'federal', value: 'commissioner@hamilton.state.gov' },
+            ],
+            name: 'Hamilton State Election Commission',
+            phone: [
+                { annotation: 'domestic', value: '123-456-7890' },
+                { annotation: 'international', value: '+1-123-456-7890' },
+            ],
+        },
+        contests: [
+            {
+                '@type': 'CandidateContest',
+                ballot_selections: [
+                    {
+                        candidate_id: 'barchi-hallaren',
+                        object_id: 'barchi-hallaren-selection',
+                        sequence_order: 0,
+                    },
+                    {
+                        candidate_id: 'cramer-vuocolo',
+                        object_id: 'cramer-vuocolo-selection',
+                        sequence_order: 1,
+                    },
+                    {
+                        candidate_id: 'court-blumhardt',
+                        object_id: 'court-blumhardt-selection',
+                        sequence_order: 2,
+                    },
+                    {
+                        candidate_id: 'boone-lian',
+                        object_id: 'boone-lian-selection',
+                        sequence_order: 3,
+                    },
+                ],
+                ballot_subtitle: {
+                    text: [
+                        { language: 'en', value: 'Vote for one' },
+                        { language: 'es', value: 'Votar por uno' },
+                    ],
+                },
+                ballot_title: {
+                    text: [
+                        {
+                            language: 'en',
+                            value: 'President and Vice President of the United States',
+                        },
+                        {
+                            language: 'es',
+                            value: 'Presidente y Vicepresidente de los Estados Unidos',
+                        },
+                    ],
+                },
+                electoral_district_id: 'hamilton-county',
+                name: 'President and Vice President of the United States',
+                number_elected: 1,
+                object_id: 'president-vice-president-contest',
+                sequence_order: 0,
+                vote_variation: 'one_of_m',
+                votes_allowed: 1,
+            },
+        ],
+    };
+}
+
 export const getManifest = async (manifest_hash: string): Promise<Manifest[] | undefined> => [
     {
         manifest_hash,
-        manifest: {
-            ballot_styles: [
-                {
-                    geopolitical_unit_ids: ['hamilton-county', 'congress-district-7'],
-                    object_id: 'congress-district-7-hamilton-county',
-                },
-            ],
-            candidates: [
-                {
-                    name: {
-                        text: [{ language: 'en', value: 'Joseph Barchi and Joseph Hallaren' }],
-                    },
-                    object_id: 'barchi-hallaren',
-                    party_id: 'whig',
-                },
-                {
-                    name: { text: [{ language: 'en', value: 'Adam Cramer and Greg Vuocolo' }] },
-                    object_id: 'cramer-vuocolo',
-                    party_id: 'federalist',
-                },
-            ],
-            contact_information: {
-                address_line: ['1234 Paul Revere Run', 'Hamilton, Ozark 99999'],
-                email: [
-                    { annotation: 'press', value: 'inquiries@hamilton.state.gov' },
-                    { annotation: 'federal', value: 'commissioner@hamilton.state.gov' },
-                ],
-                name: 'Hamilton State Election Commission',
-                phone: [
-                    { annotation: 'domestic', value: '123-456-7890' },
-                    { annotation: 'international', value: '+1-123-456-7890' },
-                ],
-            },
-            contests: [
-                {
-                    '@type': 'CandidateContest',
-                    ballot_selections: [
-                        {
-                            candidate_id: 'barchi-hallaren',
-                            object_id: 'barchi-hallaren-selection',
-                            sequence_order: 0,
-                        },
-                        {
-                            candidate_id: 'cramer-vuocolo',
-                            object_id: 'cramer-vuocolo-selection',
-                            sequence_order: 1,
-                        },
-                        {
-                            candidate_id: 'court-blumhardt',
-                            object_id: 'court-blumhardt-selection',
-                            sequence_order: 2,
-                        },
-                        {
-                            candidate_id: 'boone-lian',
-                            object_id: 'boone-lian-selection',
-                            sequence_order: 3,
-                        },
-                    ],
-                    ballot_subtitle: {
-                        text: [
-                            { language: 'en', value: 'Vote for one' },
-                            { language: 'es', value: 'Votar por uno' },
-                        ],
-                    },
-                    ballot_title: {
-                        text: [
-                            {
-                                language: 'en',
-                                value: 'President and Vice President of the United States',
-                            },
-                            {
-                                language: 'es',
-                                value: 'Presidente y Vicepresidente de los Estados Unidos',
-                            },
-                        ],
-                    },
-                    electoral_district_id: 'hamilton-county',
-                    name: 'President and Vice President of the United States',
-                    number_elected: 1,
-                    object_id: 'president-vice-president-contest',
-                    sequence_order: 0,
-                    vote_variation: 'one_of_m',
-                    votes_allowed: 1,
-                },
-            ],
-        },
+        manifest: generateMockElectionManifest(),
     },
 ];
 
@@ -104,92 +108,7 @@ export const findManifest = async (
 ): Promise<Manifest[] | undefined> => [
     {
         manifest_hash: 'F50393532F36544827661DF2279755CFFA079D32C6E0A8E8570D1D91FBAAD245',
-        manifest: {
-            ballot_styles: [
-                {
-                    geopolitical_unit_ids: ['hamilton-county', 'congress-district-7'],
-                    object_id: 'congress-district-7-hamilton-county',
-                },
-            ],
-            candidates: [
-                {
-                    name: {
-                        text: [{ language: 'en', value: 'Joseph Barchi and Joseph Hallaren' }],
-                    },
-                    object_id: 'barchi-hallaren',
-                    party_id: 'whig',
-                },
-                {
-                    name: { text: [{ language: 'en', value: 'Adam Cramer and Greg Vuocolo' }] },
-                    object_id: 'cramer-vuocolo',
-                    party_id: 'federalist',
-                },
-            ],
-            contact_information: {
-                address_line: ['1234 Paul Revere Run', 'Hamilton, Ozark 99999'],
-                email: [
-                    { annotation: 'press', value: 'inquiries@hamilton.state.gov' },
-                    { annotation: 'federal', value: 'commissioner@hamilton.state.gov' },
-                ],
-                name: 'Hamilton State Election Commission',
-                phone: [
-                    { annotation: 'domestic', value: '123-456-7890' },
-                    { annotation: 'international', value: '+1-123-456-7890' },
-                ],
-            },
-            contests: [
-                {
-                    '@type': 'CandidateContest',
-                    ballot_selections: [
-                        {
-                            candidate_id: 'barchi-hallaren',
-                            object_id: 'barchi-hallaren-selection',
-                            sequence_order: 0,
-                        },
-                        {
-                            candidate_id: 'cramer-vuocolo',
-                            object_id: 'cramer-vuocolo-selection',
-                            sequence_order: 1,
-                        },
-                        {
-                            candidate_id: 'court-blumhardt',
-                            object_id: 'court-blumhardt-selection',
-                            sequence_order: 2,
-                        },
-                        {
-                            candidate_id: 'boone-lian',
-                            object_id: 'boone-lian-selection',
-                            sequence_order: 3,
-                        },
-                    ],
-                    ballot_subtitle: {
-                        text: [
-                            { language: 'en', value: 'Vote for one' },
-                            { language: 'es', value: 'Votar por uno' },
-                        ],
-                    },
-                    ballot_title: {
-                        text: [
-                            {
-                                language: 'en',
-                                value: 'President and Vice President of the United States',
-                            },
-                            {
-                                language: 'es',
-                                value: 'Presidente y Vicepresidente de los Estados Unidos',
-                            },
-                        ],
-                    },
-                    electoral_district_id: 'hamilton-county',
-                    name: 'President and Vice President of the United States',
-                    number_elected: 1,
-                    object_id: 'president-vice-president-contest',
-                    sequence_order: 0,
-                    vote_variation: 'one_of_m',
-                    votes_allowed: 1,
-                },
-            ],
-        },
+        manifest: generateMockElectionManifest(),
     },
 ];
 
