e2dd0ac51bfd98a09e0608668316c5beb5ec29f2
Keith Fung <keith.robert.fung@gmail.com>
2021-11-23 09:55:37 -0500

    ✅ Fix menu option export

--- packages/library/src/components/MenuOption/index.ts
+++ packages/library/src/components/MenuOption/index.ts
@@ -1,10 +1,4 @@
-export { default as MenuOption } from './MenuOption';
 export * from './MenuOption';
-
-export { default as MenuOptions } from './MenuOptions';
 export * from './MenuOptions';
-
-export { default as MenuOptionType } from './MenuOptionType';
-
-export { default as TypedMenuOption } from './TypedMenuOption';
+export * from './MenuOptionType';
 export * from './TypedMenuOption';
