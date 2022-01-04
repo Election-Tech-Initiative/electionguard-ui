86b6ed1226d1ac30b83a45e20dfc53ca7bdc8e3a
SteveMaier-IRT <steve.maier@infernored.com>
2022-01-04 12:59:45 -0500

    fix unit testing

--- packages/admin-app/src/App.test.tsx
+++ packages/admin-app/src/App.test.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { IntlProvider } from 'react-intl';
+import { createTheme, CssBaseline, MuiThemeProvider } from '@material-ui/core';
 import App from './App';
+import en from './lang/en.json';
 
 test('renders learn react link', () => {
+    const queryClient = new QueryClient();
+
     render(
-        <div>
-            <App />
-        </div>
+        <React.StrictMode>
+            <QueryClientProvider client={queryClient}>
+                <IntlProvider locale="en" messages={en}>
+                    <MuiThemeProvider theme={createTheme()}>
+                        <CssBaseline />
+                        <App />
+                    </MuiThemeProvider>
+                </IntlProvider>
+            </QueryClientProvider>
+        </React.StrictMode>
     );
 });
