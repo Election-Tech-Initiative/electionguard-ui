73408d110522b16c323ec829a2950bae52c38f4f
Lee Richardson <github@leerichardson.com>
2022-01-06 18:23:47 -0500

    Revert code back to how it was

--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -1,7 +1,5 @@
+export * from './api';
 export * from './data';
 export * from './models';
-
-import * as Api from './api/index';
-export { Api };
 
 export { default as ApiClientFactory } from './api/ApiClientFactory';
