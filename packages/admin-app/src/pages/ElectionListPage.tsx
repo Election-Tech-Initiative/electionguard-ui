0b17d3f856f2e7c5b2290d7020c078305c5935d7
Lee Richardson <github@leerichardson.com>
2022-01-12 12:07:18 -0500

    Cleaned up CSS per PR review

--- packages/admin-app/src/pages/ElectionListPage.tsx
+++ packages/admin-app/src/pages/ElectionListPage.tsx
@@ -1,4 +1,4 @@
-import { ApiClientFactory, Election } from '@electionguard/api-client';
+import { ApiClientFactory, Election, ElectionState } from '@electionguard/api-client';
 import { Grid, Container, makeStyles } from '@material-ui/core';
 import { DataGrid, GridColDef } from '@material-ui/data-grid';
 import React, { useEffect, useState } from 'react';
@@ -9,20 +9,21 @@ import InternationalText from '../components/InternationalText';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 0.75,
+        paddingTop: theme.spacing(1),
+        paddingBottom: theme.spacing(3),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        minHeight: 500,
+        height: '100%',
+        width: '100%',
     },
     title: {
         fontSize: 40,
-        textAlign: 'center',
-        display: 'block',
         paddingBottom: theme.spacing(2),
     },
-    content: {
-        paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4),
-    },
     grid: {
-        minHeight: 200,
+        width: '100%',
     },
     electionState: {
         textTransform: 'lowercase',
@@ -33,6 +34,21 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+const getFakeElections = () => {
+    const state: ElectionState = 'OPEN' as ElectionState;
+    return Array(100)
+        .fill(undefined)
+        .map(
+            (a, i) =>
+                ({
+                    election_id: `election_${i}`,
+                    key_name: `key_ceremony_${i}`,
+                    state,
+                    context: undefined,
+                    manifest: undefined,
+                } as Election)
+        );
+};
 export const ElectionListPage: React.FC = () => {
     const initialElections: Election[] = [];
     const [elections, setElections] = useState(initialElections);
@@ -48,7 +64,6 @@ export const ElectionListPage: React.FC = () => {
                 id: 'election_list_page.election_id_header',
             }),
             width: 300,
-            headerClassName: 'bold-style--header',
         },
         {
             field: 'key_name',
@@ -56,7 +71,6 @@ export const ElectionListPage: React.FC = () => {
                 id: 'election_list_page.key_name_header',
             }),
             width: 300,
-            headerClassName: 'bold-style--header',
         },
         {
             field: 'state',
@@ -64,7 +78,6 @@ export const ElectionListPage: React.FC = () => {
                 id: 'election_list_page.state_header',
             }),
             width: 150,
-            headerClassName: 'bold-style--header',
             cellClassName: classes.electionState,
         },
     ];
@@ -72,7 +85,10 @@ export const ElectionListPage: React.FC = () => {
     const getElections = async () => {
         const service = ApiClientFactory.getMediatorApiClient();
         try {
-            const electionResults = await service.findElection({}, 0, 100);
+            const electionResults1 = await service.findElection({}, 0, 100);
+            const electionResults2 = getFakeElections();
+            const useMock = false;
+            const electionResults = useMock ? electionResults2 : electionResults1;
             if (electionResults) {
                 setElections(electionResults);
             }
@@ -84,31 +100,29 @@ export const ElectionListPage: React.FC = () => {
         getElections();
     }, []);
     return (
-        <Grid container className={classes.root}>
-            <Container maxWidth="md" className={classes.content}>
-                <InternationalText className={classes.title} id="election_list_page.title" />
+        <Container maxWidth="md" className={classes.root}>
+            <InternationalText className={classes.title} id="election_list_page.title" />
 
-                <div className={classes.navArea}>
-                    <GoHomeButton id="election_list_page.go_home" />
-                </div>
-                <DataGrid
-                    rows={elections}
-                    columns={columns()}
-                    getRowId={(r) => r.election_id}
-                    components={{
-                        Toolbar: FilterToolbar,
-                    }}
-                    disableSelectionOnClick
-                    className={classes.grid}
-                    pageSize={pageSize}
-                    onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-                    rowsPerPageOptions={[10, 50, 100]}
-                    pagination
-                />
-                {elections ? <div /> : <div>No elections found</div>}
-                <div>{textResult}</div>
-            </Container>
-        </Grid>
+            <div className={classes.navArea}>
+                <GoHomeButton id="election_list_page.go_home" />
+            </div>
+            <DataGrid
+                rows={elections}
+                columns={columns()}
+                getRowId={(r) => r.election_id}
+                components={{
+                    Toolbar: FilterToolbar,
+                }}
+                disableSelectionOnClick
+                className={classes.grid}
+                pageSize={pageSize}
+                onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                rowsPerPageOptions={[10, 50, 100]}
+                pagination
+            />
+            {elections ? <div /> : <div>No elections found</div>}
+            <div>{textResult}</div>
+        </Container>
     );
 };
 
