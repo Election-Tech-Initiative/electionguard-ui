0b0d9322f7230fa9c3e216b045004b1291c7fc95
Lee Richardson <github@leerichardson.com>
2022-01-13 10:36:47 -0500

    Auth working with mock call

--- packages/admin-app/src/pages/LoginPage.tsx
+++ packages/admin-app/src/pages/LoginPage.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, Container, InputAdornment, makeStyles, TextField } from '@material-ui/core';
+import { ApiClientFactory } from '@electionguard/api-client';
+import { Button, Container, InputAdornment, makeStyles, TextField } from '@material-ui/core';
 import { AccountCircle, Lock } from '@material-ui/icons';
-import React from 'react';
+import React, { useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -20,40 +21,63 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export const LoginPage: React.FC = () => {
+export interface LoginPageProps {
+    setToken: (token: string) => void;
+}
+
+export const LoginPage: React.FC<LoginPageProps> = ({ setToken }) => {
     const classes = useStyles();
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault();
+        const service = ApiClientFactory.getMediatorApiClient();
+        console.log(`logging in with ${username} and ${password}`);
+        setToken('[success token]');
+    };
+
     return (
         <Container maxWidth="xs" className={classes.root}>
-            <TextField
-                id="username"
-                label="Username"
-                variant="standard"
-                className={classes.text}
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <AccountCircle />
-                        </InputAdornment>
-                    ),
-                }}
-            />
-            <TextField
-                id="password"
-                label="Password"
-                variant="standard"
-                className={classes.text}
-                type="password"
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <Lock />
-                        </InputAdornment>
-                    ),
-                }}
-            />
-            <Button variant="contained" color="primary" className={classes.submit}>
-                Log In
-            </Button>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    id="username"
+                    label="Username"
+                    variant="standard"
+                    className={classes.text}
+                    onChange={(e) => setUserName(e.target.value)}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <AccountCircle />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <TextField
+                    id="password"
+                    label="Password"
+                    variant="standard"
+                    className={classes.text}
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <Lock />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                >
+                    Log In
+                </Button>
+            </form>
         </Container>
     );
 };
--- packages/admin-app/src/routes/MainRoutes.tsx
+++ packages/admin-app/src/routes/MainRoutes.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import {
@@ -22,27 +22,34 @@ import routeIds from './RouteIds';
  * The routes to display when the user is fully authenticated
  * and able to view the main UI
  */
-const MainRoutes: React.FC = () => (
-    <Routes>
-        <Route path={routeIds.home} element={<Navigate to="/menu" />} />
-        <Route path="/menu" element={<MenuPage />} />
+const MainRoutes: React.FC = () => {
+    const [token, setToken] = useState<string>();
 
-        <Route path="/election" element={<ElectionListPage />} />
-        <Route path="/election-setup" element={<ElectionSetupPage />} />
-        <Route path="/election/:election-id/key" element={<ElectionKeyPage />} />
-        <Route path="/election/:election-id/upload-ballot" element={<UploadBallotPage />} />
+    if (!token) {
+        return <LoginPage setToken={setToken} />;
+    }
 
-        <Route path="/key" element={<KeyListPage />} />
-        <Route path="/key-setup" element={<KeySetupPage />} />
-        <Route path="/key/:key-id/ceremony" element={<KeyCeremonyPage />} />
+    return (
+        <Routes>
+            <Route path={routeIds.home} element={<Navigate to="/menu" />} />
+            <Route path="/menu" element={<MenuPage />} />
 
-        <Route path="/tally" element={<TallyListPage />} />
-        <Route path="/tally-setup" element={<TallySetupPage />} />
-        <Route path="/tally/:key-id/ceremony" element={<TallyCeremonyPage />} />
+            <Route path="/election" element={<ElectionListPage />} />
+            <Route path="/election-setup" element={<ElectionSetupPage />} />
+            <Route path="/election/:election-id/key" element={<ElectionKeyPage />} />
+            <Route path="/election/:election-id/upload-ballot" element={<UploadBallotPage />} />
 
-        <Route path="/manage-users" element={<UserManagementPage />} />
-        <Route path="/login" element={<LoginPage />} />
-    </Routes>
-);
+            <Route path="/key" element={<KeyListPage />} />
+            <Route path="/key-setup" element={<KeySetupPage />} />
+            <Route path="/key/:key-id/ceremony" element={<KeyCeremonyPage />} />
+
+            <Route path="/tally" element={<TallyListPage />} />
+            <Route path="/tally-setup" element={<TallySetupPage />} />
+            <Route path="/tally/:key-id/ceremony" element={<TallyCeremonyPage />} />
+
+            <Route path="/manage-users" element={<UserManagementPage />} />
+        </Routes>
+    );
+};
 
 export default MainRoutes;
