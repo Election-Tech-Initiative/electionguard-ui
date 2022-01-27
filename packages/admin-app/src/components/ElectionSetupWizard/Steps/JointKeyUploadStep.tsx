3dc7f859ce92241c927660ac1e7ec34dcced187d
Lee Richardson <github@leerichardson.com>
2022-01-27 15:58:10 -0500

    Read uploaded file

--- packages/admin-app/src/components/ElectionSetupWizard/Steps/JointKeyUploadStep.tsx
+++ packages/admin-app/src/components/ElectionSetupWizard/Steps/JointKeyUploadStep.tsx
@@ -1,7 +1,7 @@
-import { Box, Button, Container } from '@mui/material';
+import { Box, Button, CircularProgress, Container } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import { VpnKey as KeyIcon } from '@mui/icons-material';
-import React from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import { SubmitElectionRequest } from '@electionguard/api-client';
@@ -13,6 +13,10 @@ const useStyles = makeStyles((theme) => ({
         flexGrow: 1,
         height: '100%',
     },
+    wrapper: {
+        margin: theme.spacing(1),
+        position: 'relative',
+    },
     content: {
         paddingTop: theme.spacing(4),
         paddingBottom: theme.spacing(4),
@@ -22,6 +26,13 @@ const useStyles = makeStyles((theme) => ({
     },
     spaced: {
         marginBottom: theme.spacing(2),
+    },
+    buttonProgress: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginTop: -12,
+        marginLeft: -12,
     },
 }));
 
@@ -34,10 +45,23 @@ export interface JointKeyUploadStepProps {
  * Joint Key Select Step for Election Setup
  */
 const JointKeyUploadStep: React.FC<JointKeyUploadStepProps> = ({ onNext }) => {
+    const [uploading, setUploading] = useState(false);
+    const [error, setError] = useState(false);
     const classes = useStyles();
 
     const onButtonClick = () => {
         onNext();
+    };
+
+    const onFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e?.target?.files?.length) {
+            console.log('zi');
+            const file = e.target.files[0];
+            const text = await file.text();
+            console.log(text);
+        } else {
+            setError(true);
+        }
     };
 
     return (
@@ -50,14 +74,38 @@ const JointKeyUploadStep: React.FC<JointKeyUploadStepProps> = ({ onNext }) => {
         >
             <Container maxWidth="md" className={classes.content}>
                 <IconHeader
-                    title={
-                        new Message(
-                            MessageId.ElectionSetup_JointKeyUpload_Title,
-                            'Joint Key Retrieved'
-                        )
-                    }
+                    title={new Message(MessageId.ElectionSetup_JointKeyUpload_Title)}
                     Icon={KeyIcon}
                 />
+
+                <div className={classes.wrapper}>
+                    <Button
+                        disabled={uploading}
+                        color="secondary"
+                        variant="contained"
+                        component="label"
+                    >
+                        <FormattedMessage
+                            id={MessageId.ElectionSetupUploadManifestUpload}
+                            defaultMessage="Select Files to Upload"
+                        />
+                        <input
+                            id="manifest-upload"
+                            accept="application/JSON"
+                            type="file"
+                            hidden
+                            onChange={(e) => onFileUpload(e)}
+                        />
+                    </Button>
+                    {uploading && (
+                        <CircularProgress
+                            size={24}
+                            variant="indeterminate"
+                            className={classes.buttonProgress}
+                        />
+                    )}
+                </div>
+
                 <Box display="flex" flexDirection="column" alignItems="center">
                     <Button
                         className={classes.spaced}
